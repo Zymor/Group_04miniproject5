c14cdb4bed6cb12e214b6fed8c81f199530378d9
Davie <daov31501@gmail.com>
2026-06-16 14:28:45 +0700

    Create Controller.java

---
+++ Controller.java
@@ -0,0 +1,37 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private RadioButton AMS;
+
+    @FXML
+    private DatePicker BIRTHDATE;
+
+    @FXML
+    private RadioButton GIC;
+
+    @FXML
+    private RadioButton GTR;
+
+    @FXML
+    private CheckBox NETWORK;
+
+    @FXML
+    private CheckBox OOP;
+
+    @FXML
+    private CheckBox OS;
+
+    @FXML
+    private Button SUBMITBUTTON;
+
+    @FXML
+    private TextField TEXTFIEL1;
+
+}
